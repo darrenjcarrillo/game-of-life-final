afd737e926dda156dd233a738159e045f3051925
Darren Carrillo <48144231+darrenjcarrillo@users.noreply.github.com>
2020-10-25 14:53:57 -0700

    Rules and About updated

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,10 @@ const About = props => {
     <div className="about">
       <div>
         <h3>ABOUT</h3>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <p>The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.</p>
+        <p>It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input. </p>
+        <p>One interacts with the Game of Life by creating an initial configuration and observing how it evolves</p>
+        <p>It is Turing complete and can simulate a universal constructor or any other Turing machine.</p>
       </div>
     </div>
   )
--- src/components/Rules.js
+++ src/components/Rules.js
@@ -10,7 +10,10 @@ const Rules = props => {
     <div className="rules">
       <div>
         <h3>Rules</h3>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <p>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</p>
+        <p>Any live cell with two or three live neighbours lives on to the next generation.</p>
+        <p>Any live cell with more than three live neighbours dies, as if by overpopulation.</p>
+        <p>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</p>
       </div>
     </div>
   )
